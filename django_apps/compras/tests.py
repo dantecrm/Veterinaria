71d951e805d6718ca82a0d8e32c2b05180d2a4af
dantecrm <dangenius.14@hotmail.com>
2016-12-01 02:35:24 -0500

    testeos de compras e inventario

--- django_apps/compras/tests.py
+++ django_apps/compras/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+import tempfile
+from django.test import TestCase, Client
+from .models import CompraEmpresa
+from django_apps.home.models import Empleado
+from django_apps.inventario.models import Articulo, Categoria
+from django_apps.persona.models import Proveedor
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+def create_proveedor(razon_social,ruc,descripcion,direccion,contacto,email):
+    image = tempfile.NamedTemporaryFile(suffix='.jpg').name
+    return Proveedor.objects.create(razon_social=razon_social,ruc=ruc,
+                                    descripcion=descripcion,direccion=direccion,
+                                    contacto=contacto,email=email,avatar=image)
+
+def create_categoria(nombre):
+    return Categoria.objects.create(nombre=nombre)
+
+def create_producto(nombre,marca,p_compra,p_venta):
+    image = tempfile.NamedTemporaryFile(suffix='.jpg').name
+    return Articulo.objects.create(nombre = nombre, marca = marca, p_compra=p_compra,
+                                   p_venta = p_venta, avatar = image)
+
+def create_compra(proveedor,categoria,producto,cantidad,descuento):
+    return CompraEmpresa.objects.create(proveedor_id=proveedor,categoria_id=categoria,producto_id=producto,cantidad=cantidad,descuento=descuento)
+
+class OrdenCompraViewTests(TestCase):
+    def setUp(self):
+        Empleado.objects.create_user('admin','admin@gmail.com','dante2016')
+        self.client.login(username='admin',password='dante2016')
+
+    def test_create_ordencompra(self):
+        proveedor = create_proveedor(razon_social='Only Pets', ruc='23781942711',
+                                     descripcion='Esta empresa es una veterinaria', direccion='Jr. Junin # 328',
+                                     contacto='Hector Caira Mamani',email='veterinariaonlypetscanissur@gmail.com')
+        categoria = create_categoria(nombre='Alimentacion')
+        producto = create_producto(nombre='Croquetas de Maiz',marca='RicoCan',p_compra=30,p_venta=40)
+        compra = create_compra(proveedor=proveedor.id,categoria=categoria.id,producto=producto.id,cantidad=60,descuento=10.00)
+        self.assertEqual(compra.pk, 1)
+        # response = self.client.get('compras:listar_compras',follow=True)
+        # print(response)
+        # self.assertQuerysetEqual(response.context['listar_compras'], ['<CompraEmpresa: Croquetas de Maiz>'])
+
+    def test_detail_ordencompra(self):
+        proveedor = create_proveedor(razon_social='Only Pets', ruc='23781942711',
+                                     descripcion='Esta empresa es una veterinaria', direccion='Jr. Junin # 328',
+                                     contacto='Hector Caira Mamani',email='veterinariaonlypetscanissur@gmail.com')
+        categoria = create_categoria(nombre='Alimentacion')
+        producto = create_producto(nombre='Croquetas de Maiz',marca='RicoCan',p_compra=30,p_venta=40)
+        compra = create_compra(proveedor=proveedor.id,categoria=categoria.id,producto=producto.id,cantidad=60,descuento=10.00)
+        # print(compra.id)
+        url = reverse('compras:detalle_compra',args=(compra.id,))
+        response = self.client.get(url,follow=True)
+        self.assertEqual(200,response.status_code)
+
+    def test_delete_ordencompra(self):
+        c = Client()
+        proveedor = create_proveedor(razon_social='Only Pets', ruc='23781942711',
+                                     descripcion='Esta empresa es una veterinaria', direccion='Jr. Junin # 328',
+                                     contacto='Hector Caira Mamani',email='veterinariaonlypetscanissur@gmail.com')
+        categoria = create_categoria(nombre='Alimentacion')
+        producto = create_producto(nombre='Croquetas de Maiz',marca='RicoCan',p_compra=30,p_venta=40)
+        compra = create_compra(proveedor=proveedor.id,categoria=categoria.id,producto=producto.id,cantidad=60,descuento=10.00)
+        url = reverse('compras:eliminar_compra',args=(compra.id,))
+        response =self.client.get(url,follow=True,secure=True)
+        # print(response)
+        self.assertEqual(200,response.status_code)
--- django_apps/inventario/tests.py
+++ django_apps/inventario/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from django.test import TestCase, Client
+import tempfile
+from .models import Articulo, Categoria
+from django_apps.home.models import Empleado
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+def create_articulo(nombre, marca,p_compra,p_venta):
+    image = tempfile.NamedTemporaryFile(suffix='.jpg').name
+    return Articulo.objects.create(nombre = nombre, marca = marca, p_compra=p_compra,
+                                   p_venta = p_venta, avatar = image)
+
+class ArticuloViewTests(TestCase):
+    def setUp(self):
+        user = Empleado.objects.create_user('admin','admin@gmail.com','dante2016')
+        self.client.login(username='admin',password='dante2016')
+
+    def test_status_article(self):
+        response = self.client.get(reverse('inventario:listar_articulos'), follow=True)
+        user = Empleado.objects.get(username='admin')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_article(self):
+        create_articulo(nombre='Comida de Mascota',marca='RicoCan',p_compra=20,p_venta=30)
+        response = self.client.get(reverse('inventario:listar_articulos'),follow=True)
+        self.assertQuerysetEqual(response.context['listar_articulos'], ['<Articulo: Comida de Mascota>'])
+
+    def test_detalle_articulo(self):
+        c = Client()
+        a = create_articulo(nombre='Comida de Mascota',marca='RicoCan',p_compra=20,p_venta=30)
+        url = reverse('inventario:detalle_articulo',args=(a.id,))
+        # c.login(username='admin',password='dante2016')
+        response = c.get(url,follow=True)
+        self.assertEqual(response.status_code, 200)
+        # self.assertContains(response, a.nombre)
+
+    def test_delete_articulo(self):
+        c = Client()
+        a = create_articulo(nombre='Comida de Mascota',marca='RicoCan',p_compra=20,p_venta=30)
+        url = reverse('inventario:eliminar_articulo',args=(a.id,))
+        response = c.delete(url,follow=True)
+        self.assertEqual(200,response.status_code)
+
+def create_categoria(nombre):
+    return Categoria.objects.create(nombre=nombre)
+
+class CategoriaViewTests(TestCase):
+    def setUp(self):
+        user = Empleado.objects.create_user('admin','admin@gmail.com','dante2016')
+        self.client.login(username='admin',password='dante2016')
+
+    def test_status_categoria(self):
+        response = self.client.get(reverse('inventario:listar_categorias'), follow=True)
+        user = Empleado.objects.get(username='admin')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_categoria(self):
+        create_categoria(nombre='Accesorios')
+        response = self.client.get(reverse('inventario:listar_categorias'),follow=True)
+        self.assertQuerysetEqual(response.context['listar_categorias'], ['<Categoria: Accesorios>'])
+
+    def test_delete_categoria(self):
+        c = Client()
+        a = create_categoria(nombre='Alimento')
+        url = reverse('inventario:eliminar_categoria',args=(a.id,))
+        response = c.delete(url,follow=True)
+        self.assertEqual(200,response.status_code)
